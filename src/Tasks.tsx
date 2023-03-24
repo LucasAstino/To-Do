97a3a9cb58db7232bbbd8e507c919c4962f67c82
LUCAS ASTINO <lucasastino2@gmail.com>
2023-03-24 21:14:22 +0000

    update

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,31 +1,25 @@
-import { useState } from "react"
-import"./Tasks.css"
-import Clipboard from "./assets/Clipboard.svg"
-
-export function Tasks() {
-
-    const [tasks,setTasks]=useState([{}])
-
-    const oi = `<div className="notTasks">
-    <p><strong>
-    Você ainda não tem tarefas cadastradas
-    </strong><br/>
-    Crie tarefas e organize seus itens a fazer
-    </p>
-</div>"`
+import React, { useState } from "react"
 
 
-return(
-<section className="tasksContainer">
-    <header className="headerTasks">
-        <p className="created">Tarefas criadas <span>0</span></p>
-        <p className="done">Concluídas <span>0</span></p>
-    </header>
-    <main className="main">
-        <img src={Clipboard} alt="Clipboard" />
-        {oi}
-    </main>
-</section>
-  
-)
+interface EnumServiceGetOrderBy {
+  teste:Array<{
+    task:{
+    check:boolean,
+    content:string
+  }
+  }>
+}
+
+export default function Tasks({teste}:EnumServiceGetOrderBy){
+
+
+console.log(teste)
+    
+    return(
+        <div>    
+         {teste.map(t =>{
+          <h1>oi</h1>
+         })} 
+        </div>
+    )
 }
